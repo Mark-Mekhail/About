3a63d94416c271fea186a1842386f6d16e1d1ecd
Mark-Mekhail <markm2hs@gmail.com>
2024-11-25 15:43:10 -0800

    Rough interim changes

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 // Required images
 import markSeattle from "../images/mark-seattle.jpeg";
 import markBanff from "../images/mark-banff.jpeg";
 import markBeehiveHike from "../images/mark-beehive-hike.jpeg";
 
+// Define static constants
+const photoVariants = {  visible: { opacity: 1, scale: 1, x: [200, 0],  transition: { duration: 1 } },  hidden: { opacity: 0, scale: 1 }};
+
 const Photos = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) { 
+      controls.start("visible");    
+    }  
+  }, [controls, inView]);
+
   return (
     <section className="photos">
-      <img src={markBanff} className="top-image"/>
-      <img src={markBeehiveHike} className="central-image"/>
-      <img src={markSeattle} className="bottom-image"/>
+      <motion.div ref={ref} animate={controls} initial="hidden" variants={photoVariants} className="photos-div">
+        <img src={markBanff} className="top-image"/>
+        <img src={markBeehiveHike} className="central-image"/>
+        <img src={markSeattle} className="bottom-image"/>
+      </motion.div>
     </section>
   );
 };
