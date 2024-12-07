9f97caf5ed1a95e006b8eb0bfb575f41c0def045
Mark-Mekhail <markm2hs@gmail.com>
2024-12-06 23:14:42 -0800

    Improve cypress test consistency for CI

--- src/test/end-to-end/website.cy.js
+++ src/test/end-to-end/website.cy.js
@@ -43,7 +43,7 @@ viewportSizes.forEach((viewportSize) => {
 
     it("shows all elements after scrolling through the page", () => {
       cy.get(selectors.footer).then((element) => {
-        const scrollableHeight = element[0].offsetTop + element[0].offsetHeight;
+        const scrollableHeight = (element[0].offsetTop + element[0].offsetHeight) * 1.25;
         cy.get(element).scrollIntoView({ duration: scrollableHeight, easing: "linear" });
       });
 
