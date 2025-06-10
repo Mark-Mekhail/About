e17b39493405cd8f1e7f8d3fcda5f191778972e8
Mark-Mekhail <markm2hs@gmail.com>
2025-06-09 19:53:47 -0700

    Replace landscape image with a JPEG version and remove the old PNG file

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -9,7 +9,7 @@ import Variants from "../utils/Variants";
 
 // Required images
 import topPortraitImage from "../images/mark-banff.jpeg";
-import landscapeImage from "../images/amazon-photo.png";
+import landscapeImage from "../images/amazon-photo.jpeg";
 import bottomPortraitImage from "../images/mark-seattle.jpeg";
 
 const sectionVariants = Variants.defaultVariants();
